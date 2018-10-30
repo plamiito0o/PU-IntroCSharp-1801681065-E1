21a33c0d8c27d866246426bcaa09ff75618ff3bc
Teodora <tydi18@abv.bg>
2018-10-30 18:18:05 +0200

    Exam1

---
+++ ExamOne1801681065/Calculator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 20.78;
+            Console.WriteLine("Enter operation");
+            string operator4e = Console.ReadLine();
+            switch (operator4e)
+            {
+                case "+":
+                    double sum = a + b;
+                    double withPercentage = sum + (sum/100 * 10);
+                    Console.WriteLine($"Result is:{withPercentage}");
+                    break;
+                case "-":
+                    double min1=0;
+                    double max1=0;
+                    if (a > b)
+                    {
+                        a = max1;
+                        b = min1;
+         
+                    }
+                    double de = Math.Pow(a, 2) - b;
+                    Console.WriteLine($"Result is:{de}");
+
+
+
+
+                    break;
+                case "*":
+                    double umn = a * Math.Sqrt(b);
+                    Console.WriteLine($"Result is:{umn}");
+                    break;
+                case "/":
+                    if (b == 0)
+                    { Console.WriteLine("b=0 -- no"); }
+                    else
+                    { double del = a / b;
+                        Console.WriteLine($"Result is:{del}"); }
+                    break;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExamOne1801681065/RectangleArea/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd=new Random();
+            int NumberA = rnd.Next(1,100);
+            int NumberB = rnd.Next(1,100);
+            Console.WriteLine($"Number A: {NumberA},Number B: {NumberB}");
+            int Area = NumberA * NumberB;
+            Console.WriteLine($"The area of the rectangle is {Area:0.00}");
+            Console.ReadKey();
+        }
+    }
+}
